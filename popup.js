63a95895676877a65827780d8730251e31a14291
SebFine <62475734+SebFine@users.noreply.github.com>
2020-06-07 10:56:08 +0200

    added modal layer functinality

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 //some code to create a modal layer
+const helpBtn = document.querySelector(".helpBtn");
+const modalBg = document.querySelector(".modalBg");
 
-const helpBtn = document.querySelector(".helpBtb");
+modalBg.style.display = "none";
+
+const openHelp = () => {
+    modalBg.style.display = "block";
+};
+
+const closeHelp = () => {
+    modalBg.style.display = "none";
+}
+
+helpBtn.addEventListener('click', openHelp);
+modalBg.addEventListener('click', closeHelp);
