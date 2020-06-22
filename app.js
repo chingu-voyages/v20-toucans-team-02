fd46c6b47018c0806756f3ac88b3aa9dfc2d1a29
SebFine <62475734+SebFine@users.noreply.github.com>
2020-06-22 14:50:35 +0200

    Added functionality

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Part 1 of the game logic includes the fetch of a random definition and a correlated word, as well as 2 other random words.
 
-const showDefinition = document.querySelector("definition");
-const showWord = document.querySelector("words");
+const showDefinition = document.querySelector(".definition");
+const showWord = document.querySelector(".words");
+const list1 = document.querySelector(".definitions");
+const list2 = document.querySelector(".words");
 
 //This function gets a random word from wordnik.
 const fetchRandomWord = async () => {
@@ -32,9 +34,43 @@ const fetchDefinition = async () => {
   };
 };
 
-const Definition = fetchDefinition().then((definitions) => {
-const word = definitions.word;
-const definition = definitions.definitions[0];
+const definition = fetchDefinition().then((definitions) => {
+  console.log(definitions.definitions);
+  console.log(definitions.word);
 
+  definitions.definitions.forEach((el) => {
+    let elem = document.createElement("li");
+    elem.classList.add("definition");
+    elem.innerHTML = el;
+    list1.appendChild(elem);
+  });
 
+  const randomPos = () => {
+    const randomArr = [];
+    for (let i = 0; i < 3; i++) {
+      const random = Math.floor(Math.random() * 3);
+      randomArr.push(random);
+    }
+    console.log(`randomArr: ${randomArr}`);
+  };
+
+  let elem = document.createElement("li");
+  elem.classList.add("wordsItem");
+  elem.innerHTML = definitions.word;
+  list2.appendChild(elem);
+
+  // create an array, with 3 random positions, then somehow change the order of append child.
+
+  for (let i = 0; i < 2; i++) {
+    fetchDefinition().then((definitions) => {
+      const word = definitions.word;
+      console.log(word);
+      let elem = document.createElement("li");
+      elem.classList.add("wordsItem");
+      elem.innerHTML = word;
+      list2.appendChild(elem);
+    });
+  }
 });
+
+fetchDefinition();
