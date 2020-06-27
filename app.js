4224ec4b317e018abd31baa55a683bf57d943eac
SebFine <62475734+SebFine@users.noreply.github.com>
2020-06-27 09:00:45 +0200

    Changed structure, refactored,  getting closer

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Part 1 of the game logic includes the fetch of a random definition and a correlated word, as well as 2 other random words.
-
 const showDefinition = document.querySelector(".definition");
 const showWord = document.querySelector(".words");
 const list1 = document.querySelector(".definitions");
@@ -7,6 +6,7 @@ const list1 = document.querySelector(".definitions");
 const wordArr = [];
 
 const fetchRandomWord = async () => {
+  //todo Here must be the for 3 times loop???
   const random = await axios.get(
     "http://api.wordnik.com/v4/words.json/randomWord",
     {
@@ -16,9 +16,12 @@ const fetchRandomWord = async () => {
     }
   );
   wordArr.push(random.data.word);
+  console.log(`3 random words: ${wordArr}`);
+  return wordArr;
 };
 
 const fetchDefinition = async () => {
+  const wait = await fetchRandomWord();
   const randomWord = wordArr[Math.floor(Math.random() * 3)];
   const definitionURL = `http://api.wordnik.com/v4/word.json/${randomWord}/definitions`;
 
@@ -27,78 +30,37 @@ const fetchDefinition = async () => {
       api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
     },
   });
-console.log(wordDefinition);
+  console.log(`Chosen random word: ${randomWord}`);
   return {
     word: randomWord,
     definitions: wordDefinition.data.map((x) => x.text),
   };
 };
 
-const game = () => {
-  for (let i = 0; i < 3; i++) {
-    fetchRandomWord();
-  }
-  fetchDefinition();
-  console.log(wordArr);
-  console.log("");
-  return wordArr;
-};
+/*  */
 
-game();
-
-/* const definition = fetchDefinition().then((definitions) => {
-  const randomWord = definitions.word;
-
-  definitions.definitions.forEach((el) => {
+/* const word = fetchRandomWord().then((wordsArr) => {
+  wordsArr.forEach((el) => {
     let elem = document.createElement("li");
     elem.classList.add("definition");
     elem.innerHTML = el;
-    list1.appendChild(elem);
+    showWord.appendChild(elem);
   });
-
-  let elem = document.createElement("li");
-      elem.classList.add("wordsItem");
-      elem.innerHTML = randomWord;
-      showWord.appendChild(elem);
 }); */
 
-/*  fetchDefinition().then((definitions) => {
-      const randomWord = await fetchRandomWord();
-      wordArr.push(word);
-      console.log(wordArr); */
-
-/* let elem = document.createElement("li");
-      elem.classList.add("wordsItem");
-      elem.innerHTML = randomWord;
-      showWord.appendChild(elem); */
-
-
-//This function gets a random word from wordnik.
-/* const fetchRandomWord = async () => {
-  const random = await axios.get(
-    "http://api.wordnik.com/v4/words.json/randomWord",
-    {
-      params: {
-        api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
-      },
-    }
-  );
-  return random.data.word;
-}; */
-
-/* const fetchDefinition = async () => {
-  const randomWord = await fetchRandomWord();
-  const definitionURL = `http://api.wordnik.com/v4/word.json/${randomWord}/definitions`;
-
-  const wordDefinition = await axios.get(definitionURL, {
-    params: {
-      api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
-    },
+const game = () => {
+  for (let i = 0; i < 2; i++) {
+    fetchRandomWord();
+    console.log(i);
+  }
+  const definition = fetchDefinition().then((definitions) => {
+    definitions.definitions.forEach((el) => {
+      let elem = document.createElement("li");
+      elem.classList.add("definition");
+      elem.innerHTML = el;
+      list1.appendChild(elem);
+    });
   });
+};
 
-  return {
-    word: randomWord,
-    definitions: wordDefinition.data.map((x) => x.text),
-  };
-}; 
-*/
+game();
