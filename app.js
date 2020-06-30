bba317e673479d812eb725383f877859485c46dd
SebFine <62475734+SebFine@users.noreply.github.com>
2020-06-30 09:25:58 +0200

    Finished func, added Styling

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const showDefinition = document.querySelector(".definition");
 const showWord = document.querySelector(".words");
 const list1 = document.querySelector(".definitions");
 
+let win = false;
 const wordArr = [];
+let randomWord = "";
 
 const fetchRandomWord = async () => {
-  //todo Here must be the for 3 times loop???
   const random = await axios.get(
     "http://api.wordnik.com/v4/words.json/randomWord",
     {
@@ -22,7 +23,7 @@ const fetchRandomWord = async () => {
 
 const fetchDefinition = async () => {
   const wait = await fetchRandomWord();
-  const randomWord = wordArr[Math.floor(Math.random() * 3)];
+  randomWord = wordArr[Math.floor(Math.random() * 3)];
   const definitionURL = `http://api.wordnik.com/v4/word.json/${randomWord}/definitions`;
 
   const wordDefinition = await axios.get(definitionURL, {
@@ -30,35 +31,53 @@ const fetchDefinition = async () => {
       api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
     },
   });
-  console.log(`Chosen random word: ${randomWord}`);
   return {
     word: randomWord,
     definitions: wordDefinition.data.map((x) => x.text),
   };
 };
 
-/*  */
-
-/* const word = fetchRandomWord().then((wordsArr) => {
-  wordsArr.forEach((el) => {
-    let elem = document.createElement("li");
-    elem.classList.add("definition");
-    elem.innerHTML = el;
-    showWord.appendChild(elem);
-  });
-}); */
-
 const game = () => {
   for (let i = 0; i < 2; i++) {
     fetchRandomWord();
-    console.log(i);
   }
+
   const definition = fetchDefinition().then((definitions) => {
     definitions.definitions.forEach((el) => {
       let elem = document.createElement("li");
       elem.classList.add("definition");
       elem.innerHTML = el;
       list1.appendChild(elem);
+    });
+  });
+
+  const word = fetchRandomWord().then((wordsArr) => {
+    wordsArr.forEach((el) => {
+      console.log(wordsArr);
+      let elem = document.createElement("li");
+      elem.classList.add("wordsItem", "pointer");
+      elem.innerHTML = el;
+      elem.addEventListener("click", () => {
+        if (el === randomWord) {
+          elem.innerHTML = `${el} <br> That's right!`;
+          elem.classList.add("win");
+          elem.classList.remove("wordsItem");
+          win = true;
+          let restart = document.createElement("li");
+          restart.classList.add("win", "pointer");
+          restart.innerHTML = `Play again?`;
+          elem.insertAdjacentElement("afterend", restart);
+          restart.addEventListener("click", () => {
+            location.reload();
+          });
+        } else {
+          if (win === false) {
+            console.log("wrong");
+            elem.innerHTML = `${el} That's not correct, Try again!`;
+          }
+        }
+      });
+      showWord.appendChild(elem);
     });
   });
 };
