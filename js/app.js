9f400b831b58ebeaa828e5eaeab37077cea34759
SebFine <62475734+SebFine@users.noreply.github.com>
2020-07-07 17:02:47 +0100

    updated api request volume and details

--- js/app.js
+++ js/app.js
@@ -6,22 +6,17 @@ let win = false;
 const wordArr = [];
 let randomWord = "";
 
-const fetchRandomWord = async () => {
+const fetchDefinition = async () => {
   const random = await axios.get(
-    "https://api.wordnik.com/v4/words.json/randomWord",
+    "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=3",
     {
       params: {
         api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
       },
     }
   );
-  wordArr.push(random.data.word);
-  console.log(`3 random words: ${wordArr}`);
-  return wordArr;
-};
-
-const fetchDefinition = async () => {
-  const wait = await fetchRandomWord();
+  const wordArr = random.data.map((x) => x.word);
+  console.log(wordArr);
   randomWord = wordArr[Math.floor(Math.random() * 3)];
   const definitionURL = `https://api.wordnik.com/v4/word.json/${randomWord}/definitions`;
 
@@ -30,31 +25,29 @@ const fetchDefinition = async () => {
       api_key: "ecfgi1h25l9ial3uiqu4e201aaor7odzentfuf3ruogimb1dc",
     },
   });
+  console.log(`RandomWord: ${randomWord}`);
+  console.log(`Word Array: ${wordArr}`);
+
   return {
     word: randomWord,
     definitions: wordDefinition.data.map((x) => x.text),
+    wordArr: wordArr,
   };
 };
 
-const game = () => {
-  for (let i = 0; i < 2; i++) {
-    setTimeout(fetchRandomWord(), 1000);
-  }
-
-  const definition = fetchDefinition().then((definitions) => {
-    definitions.definitions.slice(0, 3).forEach((el) => {
-      let elem = document.createElement("li");
-      elem.classList.add("definition");
-      elem.innerHTML = el;
-      list1.appendChild(elem);
-    });
+const definition = fetchDefinition().then((definitions) => {
+  definitions.definitions.slice(0, 3).forEach((el) => {
+    let elem = document.createElement("li");
+    elem.classList.add("definition");
+    elem.innerHTML = el;
+    list1.appendChild(elem);
   });
 
-  const word = fetchRandomWord().then((wordsArr) => {
-    wordsArr.forEach((el) => {
+    definitions.wordArr.forEach((el) => {
       let elem = document.createElement("li");
       elem.classList.add("wordsItem", "pointer");
       elem.innerHTML = el;
+  
       elem.addEventListener("click", () => {
         if (el === randomWord) {
           elem.innerHTML = `${el} <br> That's right!`;
@@ -77,6 +70,3 @@ const game = () => {
       showWord.appendChild(elem);
     });
   });
-};
-
-game();
